5e5f22216a225af113b23b943dbff068b8f1aa66
Seth Allgire <seth.allgire@gmail.com>
2021-09-11 17:09:07 -0500

    holes flash randomly and stop at timer 0

--- whack-mole.js
+++ whack-mole.js
@@ -6,25 +6,16 @@ let gameTimer = document.querySelector(".seconds");
 let time = 5;
 gameTimer.innerHTML = `${time}`;
 
-// let countdown = setInterval(() => {
-//   time--;
-//   gameTimer.innerHTML = `${time}`;
-//   if (time === 0) {
-//     clearInterval(countdown);
-//   }
-// }, 1000);
-
 function countdown() {
-  setInterval(function () {
+  setInterval(() => {
     time--;
-    gameTimer.innerHTML = `${time}`;
+    gameTimer.textContent = `${time}`;
     if (time === 0) {
       clearInterval(countdown);
     }
   }, 1000);
 }
 
-// USE THIS: IT WORKS SO FAR!!!!
 const holeDivs = document.querySelectorAll(".mole-hole");
 
 function holeCycle() {
@@ -32,7 +23,76 @@ function holeCycle() {
   activeMole.style.backgroundColor = "#FF00FF";
   setTimeout(() => {
     activeMole.style.backgroundColor = "orange";
-    setTimeout(holeCycle, 500);
+    if (time <= 0) {
+      clearTimeout;
+    } else setTimeout(holeCycle, 500);
   }, 900);
 }
+
+// // // THIS TIMER STOPS AT "0"
+// countdown = setInterval(() => {
+//   time--;
+// gameTimer.innerHTML = `${time}`;
+//   if (time === 0) {
+//     clearInterval(countdown);
+//   }
+// }, 1000);
+
+// THIS TIMER WORKS WITH EVENTLISTENER
+// function countdown() {
+//   setInterval(function () {
+//     time--;
+//     gameTimer.innerHTML = `${time}`;
+//     if (time === 0) {
+//       clearInterval(countdown);
+//     }
+//   }, 1000);
+// }
+
 // holeCycle(); ???LEAVE-THIS-COMMENTED-OUT-FOREVER???
+
+// let countdown;
+// let startButton = document.querySelector("#start-button");
+// startButton.addEventListener("click", countdown);
+// startButton.addEventListener("click", holeCycle);
+
+// let gameTimer = document.querySelector(".seconds");
+// let time = 5;
+// gameTimer.innerHTML = `${time}`;
+
+// //THIS TIMER STOPS AT "0"
+// countdown = setInterval(() => {
+//   time--;
+//   gameTimer.innerHTML = `${time}`;
+//   if (time === 0) {
+//     clearInterval(countdown);
+//   }
+// }, 1000);
+
+// //THIS TIMER WORKS WITH EVENTLISTENER
+// // function countdown() {
+// //   setInterval(function () {
+// //     time--;
+// //     gameTimer.innerHTML = `${time}`;
+// //     if (time === 0) {
+// //       clearInterval(countdown);
+// //     }
+// //   }, 1000);
+// // }
+
+// // USE THIS: IT WORKS SO FAR!!!!
+
+// // holeCycle(); ???LEAVE-THIS-COMMENTED-OUT-FOREVER???
+
+// USE THIS: IT WORKS SO FAR!!!!
+// const holeDivs = document.querySelectorAll(".mole-hole");
+// let activeMole = holeDivs[Math.floor(Math.random() * holeDivs.length)];
+// function holeCycle() {
+//   activeMole.style.backgroundColor = "#FF00FF";
+//   setTimeout(() => {
+//     activeMole.style.backgroundColor = "orange";
+//     if (time === 0) {
+//       clearTimeout;
+//     } else setTimeout(holeCycle, 500);
+//   }, 900);
+// }
