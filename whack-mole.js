2c7474a14cd4c063f837a6eea803fabac2675eef
Seth Allgire <seth.allgire@gmail.com>
2021-09-13 09:37:33 -0500

    adds disabled attribute to all buttons

--- whack-mole.js
+++ whack-mole.js
@@ -26,14 +26,16 @@ expertButton.addEventListener("click", moleExpert);
 
 let resetButton = document.querySelector("#reset-button");
 resetButton.addEventListener("click", gameReset);
+resetButton.setAttribute("disabled", "");
 
 function countdown() {
   timer = setInterval(() => {
     time--;
     gameTimer.textContent = ":" + `${time}`;
+
     if (time === 0) {
       clearInterval(timer);
-      resetButton.addEventListener("click", gameReset);
+      resetButton.removeAttribute("disabled", "");
       result.innerText = `${score}` + " ";
       resultMessage.style.visibility = "visible";
     }
@@ -42,34 +44,30 @@ function countdown() {
 
 function moleNovice() {
   let activeHole = holes[Math.floor(Math.random() * holes.length)];
-  noviceButton.style.visibility = "hidden";
-  expertButton.style.visibility = "hidden";
+  noviceButton.setAttribute("disabled", "");
+  expertButton.setAttribute("disabled", "");
   activeHole.style.backgroundColor = "#f7b05b";
   activeHole.classList.add("mole");
-  resetButton.removeEventListener("click", gameReset);
   setTimeout(() => {
     activeHole.style.backgroundColor = "#555358";
     activeHole.classList.remove("mole");
     if (time === 0) {
       clearTimeout;
-      resetButton.addEventListener("click", gameReset);
     } else setTimeout(moleNovice, 700);
   }, 900);
 }
 
 function moleExpert() {
   let activeHole = holes[Math.floor(Math.random() * holes.length)];
-  noviceButton.style.visibility = "hidden";
-  expertButton.style.visibility = "hidden";
+  noviceButton.setAttribute("disabled", "");
+  expertButton.setAttribute("disabled", "");
   activeHole.style.backgroundColor = "#f7b05b";
   activeHole.classList.add("mole");
-  resetButton.removeEventListener("click", gameReset);
   setTimeout(() => {
     activeHole.style.backgroundColor = "#555358";
     activeHole.classList.remove("mole");
     if (time === 0) {
       clearTimeout;
-      resetButton.addEventListener("click", gameReset);
     } else setTimeout(moleExpert, 400);
   }, 700);
 }
@@ -88,9 +86,7 @@ function gameReset() {
   score = 0;
   scoreKeeper.textContent = `${score}`;
   gameTimer.textContent = ":" + `${time}`;
-  noviceButton.style.visibility = "visible";
-  expertButton.style.visibility = "visible";
-  noviceButton.addEventListener("click", countdown);
-  expertButton.addEventListener("click", countdown);
+  noviceButton.removeAttribute("disabled", "");
+  expertButton.removeAttribute("disabled", "");
   resultMessage.style.visibility = "hidden";
 }
